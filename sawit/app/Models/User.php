86dba5eddd0fba41562f92925e594053d3ba7f26
Afifah-Fadilah <afifahfadila123@gmail.com>
2026-09-19 21:23:15 +0700

    fitur login

--- sawit/app/Models/User.php
+++ sawit/app/Models/User.php
@@ -20,8 +20,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
         'password',
+        'role',
     ];
 
     /**
@@ -42,8 +42,9 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
+
+    
 }
